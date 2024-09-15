5a94a95858a500352384224a247b5d6a7c31390b
Linos <153276567+Linos1391@users.noreply.github.com>
2024-09-15 21:55:02 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
 import os
+import pathlib
+
+PROJECT_PATH: str = pathlib.Path(__file__).parent
 
 prompt: str = """
 python ChessAI/ChessGUI.py
@@ -7,6 +10,7 @@ python ChessAI/ChessGUI.py
 # You may add 'conda activate ..' if needed
 
 def main():
+    os.chdir(PROJECT_PATH)
     os.system(prompt)
 
 if __name__ == '__main__':
