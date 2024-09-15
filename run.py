af440a463d8e968e079c9e3895afd73b997ca578
Linos <153276567+Linos1391@users.noreply.github.com>
2024-09-15 21:28:13 +0700

    Add files via upload

---
+++ run.py
@@ -0,0 +1,13 @@
+import os
+
+prompt: str = """
+python ChessAI/ChessGUI.py
+"""
+
+# You may add 'conda activate ..' if needed
+
+def main():
+    os.system(prompt)
+
+if __name__ == '__main__':
+    main()
