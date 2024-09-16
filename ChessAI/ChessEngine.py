6041acaaa69920b19eb386404d16a5e0c7dc87b1
Linos <153276567+Linos1391@users.noreply.github.com>
2024-09-16 20:40:49 +0700

    Update ChessEngine.py

--- ChessAI/ChessEngine.py
+++ ChessAI/ChessEngine.py
@@ -1,40 +1,8 @@
-import pathlib
-import json
-from stockfish.models import Stockfish, StockfishException
-
-PROJECT_PATH: str = pathlib.Path(__file__).parent
-PROJECT_SETTING: str = PROJECT_PATH / 'setting.json'
+from stockfish.models import Stockfish
 
 class Engine:
-    def __init__(self):
-        try:
-            with open(PROJECT_SETTING, mode="r", encoding="utf-8") as read_file:
-                self.data = json.load(read_file)
-                read_file.close()
-        except:
-            print('Cannot find setting.json file. Start create new one. If the file existed but this is called. Try to make an issue on Github.')
-            
-            self.data: dict = {
-                "ChessAI": {
-                    "Engine": "",
-                    "Analyse Every Move": False,
-                    "Elo": 1350,
-                },             
-                "Stockfish": {
-                    "Debug Log File": "",
-                    "UCI_Chess960": False,
-                    "Min Split Depth": 0,
-                    "Threads": 1,
-                    "Hash": 16,
-                },
-            }
-            
-            with open(PROJECT_SETTING, mode="w", encoding="utf-8") as write_file:
-                json.dump(self.data, write_file, indent=4)
-                write_file.close()
-                
-        if self.data['ChessAI']['Engine'] == '':
-            raise StockfishException('You did not add the Stockfish path yet.')
+    def __init__(self, data):
+        self.data = data
         
         self.engine: Stockfish = Stockfish(self.data['ChessAI']['Engine'], parameters={'UCI_LimitStrength': True})
 
@@ -80,13 +48,3 @@ class Engine:
     
     def move(self, move: str) -> None:
         self.engine.make_moves_from_current_position(move)
-        
-if __name__ == '__main__':
-    engine = Engine()
-    engine.engine.set_fen_position(input('FEN: '))
-    
-    while True:
-        print(engine.engine.get_board_visual())
-        move = input('input: ')
-        print(engine.check_move(move))
-        engine.move([move])
