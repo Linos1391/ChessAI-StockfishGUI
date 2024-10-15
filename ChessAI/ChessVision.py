ce6b62833e17115d2cbee4c6bf1474f517c2acc9
Linos <153276567+Linos1391@users.noreply.github.com>
2024-10-15 11:56:48 -0400

    Update ChessVision.py

--- ChessAI/ChessVision.py
+++ ChessAI/ChessVision.py
@@ -1,5 +1,7 @@
 import os
 import pathlib
+import json
+from datetime import date
 from PIL import Image
 import pyscreeze
 from rembg import remove
@@ -7,7 +9,7 @@ from rembg import remove
 PROJECT_PATH: str = pathlib.Path(__file__).parent
 ANALYSE_PATH: str = os.path.join(PROJECT_PATH, 'Templates', 'Analyse.png') 
 
-REQUIREMENTS: tuple = ('Chessboard.png', 'Light_Color.png', 'Dark_Color.png',
+REQUIREMENTS: tuple = ('Chessboard.png', 'Light_Color.png', 'Dark_Color.png', 'Info.json',
     'Black_Bishop.png', 'Black_King.png', 'Black_Knight.png', 'Black_Pawn.png', 'Black_Queen.png', 'Black_Rook.png',
     'White_Bishop.png', 'White_King.png', 'White_Knight.png', 'White_Pawn.png', 'White_Queen.png', 'White_Rook.png',
 )
@@ -85,7 +87,7 @@ def count_templates() -> list:
             result.append(item.name)
     return result
 
-def make_chessboard_template(img: Image.Image, analyse: bool = False) -> None:
+def make_chessboard_template(img: Image.Image, analyse: bool = False) -> Image.Image:
     """
     Try to make to the background one color only.
     """
@@ -141,24 +143,25 @@ def make_chessboard_template(img: Image.Image, analyse: bool = False) -> None:
     chosen_pixel: int = int(img.size[0]/8) * 8
     img = img.resize((chosen_pixel, chosen_pixel))
     
-    if analyse:    
-        img.save(ANALYSE_PATH)
-    else:
-        img.save(TEMPLATE_PATH / 'Chessboard.png')
+    return img
                 
-def make_chesspiece_template(chessboard_image: Image.Image) -> None:
+def make_chesspiece_template(chessboard_image: Image.Image, name: str) -> None:
     """
     We will use the chessboard that made initially.
     
     **Notice:** I used rembg for fast background removal. The correction must be done by hand.
     """
+    print('+------------------------------------------------+')
+    print('| We will freeze the app so no error will appear |')
+    print('+------------------------------------------------+')
+    
     width, height = chessboard_image.size
     
     # I have to separate 64 cells
     width_pix = width / 8
     height_pix = height / 8
     
-    print('Begin to make chess pieces\' templates')
+    print('Making chess pieces\' templates:')
     for (locX, locY, name, index) in (
             (7, 0, 'Black_Rook.png', 1), 
             (1, 0, 'Black_Knight.png', 2), 
@@ -184,7 +187,37 @@ def make_chesspiece_template(chessboard_image: Image.Image) -> None:
             img = remove(img)
         img.save(TEMPLATE_PATH / name)
         _print_progress_bar(index, 14)
+    
+    print('+-----------------------------------------------------------------------------------+')
+    print('| Create O.P and I.P, will take lots of time. Grab some coffee or do something else |')
+    print('+-----------------------------------------------------------------------------------+')
+    
+    success, OP, IP, pattern = create_OP_and_IP()
+    if not success:
+        print('OP and IP are not valid, please edit the template. OP and IP will be set to -5 and +10, respectively')
+    
+    info: dict = {
+        "name": name,
+        "date": date.today().strftime('%d/%m/%Y'),
+        "O.P": f'-{OP}',
+        "I.P": f'+{IP}',
+        "pattern": pattern
+    }
+    
+    with open(TEMPLATE_PATH / 'Info.json', mode="w", encoding="utf-8") as write_file:
+        json.dump(info, write_file, indent=4)
+        write_file.close()
 
+def _get_info_value() -> tuple:
+    try:
+        with open(TEMPLATE_PATH / 'Info.json', mode="r", encoding="utf-8") as read_file:
+            info: dict = json.load(read_file)
+            read_file.close()
+    except:
+        raise Exception('Cannot find Info.json file. This should not be possible unless you touched it. Please delete your template and create a new one.')
+    
+    return (value for value in info.values())
+    
 def analyse_chess_pieces(img: Image.Image,) -> list[list]:
     """
     Analyse chess pieces through provided chessboard image.
@@ -196,11 +229,7 @@ def analyse_chess_pieces(img: Image.Image,) -> list[list]:
         
         return (img, img.getpixel((int(width/2), int(height/2))))
     
-    def label_chess_pieces(img: Image.Image) -> str:
-        """
-        TODO - Make it more reliable.
-        """
-        
+    def label_chess_pieces(OP: int, IP: int, img: Image.Image) -> str:
         width, height = img.size
         
         light_cell, light_color = get_color('Light_Color.png', width, height)
@@ -226,10 +255,10 @@ def analyse_chess_pieces(img: Image.Image,) -> list[list]:
                         continue
                     
                     if img.getpixel((locX, locY)) in (light_color, dark_color):
-                        pattern -= 0.5
+                        pattern += OP # OP is a negative number
                     
                     if img.getpixel((locX, locY)) == template.getpixel((locX, locY)):
-                        pattern += 1
+                        pattern += IP
                     
             if pattern > max_same_pattern[0]:
                 max_same_pattern = [pattern, name]
@@ -243,12 +272,14 @@ def analyse_chess_pieces(img: Image.Image,) -> list[list]:
 
     board: list[list] = []
     
+    _, _, OP, IP = _get_info_value()
+    
     print('Begin to analyse chessboard')
     for locY in range(8):
         small_board: list = []
         for locX in range(8):
             _print_progress_bar(locX + 8*locY + 1, 64)
-            key = label_chess_pieces(
+            key = label_chess_pieces(OP, IP,
                 img.crop((
                     locX * width_pix + 1,
                     locY * height_pix + 1,
@@ -261,8 +292,102 @@ def analyse_chess_pieces(img: Image.Image,) -> list[list]:
         board.append(small_board)
     return board
 
-# img = Image.open(TEMPLATE_PATH / 'Screenshot 2024-10-08 143031.png')
-# make_chessboard_template(img)
+def create_OP_and_IP() -> tuple:
+    PIECES: tuple = (
+        "White_Pawn.png",
+        "White_Bishop.png",
+        "White_Knight.png",
+        "White_Rook.png",
+        "White_Queen.png",
+        "White_King.png",
+        "Black_Pawn.png",
+        "Black_Bishop.png",
+        "Black_Knight.png",
+        "Black_Rook.png",
+        "Black_Queen.png",
+        "Black_King.png",
+    )
+    
+    width, height = Image.open(TEMPLATE_PATH / PIECES[0]).size
+    lookalike_pattern: dict = {} # there might be those pieces that look the same. We will skip that (At lease can edit chessboard by our own hand)
+    
+    exit: bool = False
+    IP: int  = 0
+    while IP < 50 and not exit:
+        for OP in range(IP):
+            if exit:
+                break
+            
+            max_same_pattern: list[int, str] = [-100000, '']
+            
+            for main_piece in PIECES:
+                img = Image.open(TEMPLATE_PATH / main_piece)
+                
+                for template_piece in PIECES:
+                    template = Image.open(TEMPLATE_PATH / template_piece)
+                    
+                    pattern: int = 0
+                    
+                    for locY in range(height):
+                        for locX in range(width):
+
+                            if (0, 0, 0, 0) in (img.getpixel((locX, locY)), template.getpixel((locX, locY))):
+                                continue
+                            
+                            if template.getpixel((locX, locY)) != (0, 0, 0, 0):
+                                pattern -= OP # OP is not a negative number yet
+
+                            if img.getpixel((locX, locY)) == template.getpixel((locX, locY)):
+                                pattern += IP
+
+                    if pattern > max_same_pattern[0]:
+                        max_same_pattern = [pattern, template_piece]
+                                 
+                if main_piece == "Black_King.png": # it has reach the end -> success
+                    exit = True
+                    break
+                
+                if main_piece != max_same_pattern[1]:
+                    check_lookalike = f'{main_piece}_{max_same_pattern[1]}'
+                    
+                    try:
+                        if lookalike_pattern[check_lookalike] == 25:
+                            continue
+                    except:
+                        pass # ignore them
+                    
+                    if check_lookalike in lookalike_pattern.keys():
+                        lookalike_pattern.update({check_lookalike: lookalike_pattern[check_lookalike]+1})
+                    else:
+                        lookalike_pattern.update({check_lookalike: 1})
+                    
+                    OP_write: str = f'{OP}' if OP > 9 else f' {OP}' 
+                    IP_write: str = f'{IP}' if IP > 9 else f' {IP}' 
+                    print(f'OP: {OP_write}, IP: {IP_write}. Result: Fail')
+                    max_same_pattern: list[int, str] = [-100000, '']
+                    break 
+                
+             
+        IP += 1
+    
+    pattern: dict = {}
+    
+    for key, value in lookalike_pattern.items():
+        if value < 25:
+            continue
+        
+        piece_1_before, piece_1_after, piece_2_before, piece_2_after = key.split('_')
+        piece_1 = ' '.join([piece_1_before, piece_1_after])[:-4]
+        piece_2 = ' '.join([piece_2_before, piece_2_after])[:-4]
+        print(f'In the process, find "{piece_1}" and "{piece_2}" has the same pattern.')
+        pattern.update({piece_1: piece_2})
+    
+    if IP == 50:
+        return (False, 5, 10, pattern)
+    OP_write: str = f'{OP}' if OP > 9 else f' {OP}' 
+    IP_write: str = f'{IP}' if IP > 9 else f' {IP}' 
+    print(f'OP: {OP_write}, IP: {IP_write}. Result: Success')
+    return (True, OP, IP, pattern)
 
 if __name__ == '__main__':
     print('Use "main.py" dude')
